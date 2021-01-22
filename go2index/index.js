c5b6f7e479e70e9543349527c61ff625816dee73
jinoo2005609 <69671333+jinoo2005609@users.noreply.github.com>
2021-01-22 22:20:40 +0900

    Update index.js

--- go2index/index.js
+++ go2index/index.js
@@ -1,6 +1,6 @@
 // =======Options START=======
 var authConfig = {
-  siteName: "GoIndex-theme-acrou", // 网站名称
+  siteName: "Google Drive [esherloon.ga]", // 网站名称
   version: "1.1.2", // 程序版本
   theme: "acrou",
   // 强烈推荐使用自己的 client_id 和 client_secret
